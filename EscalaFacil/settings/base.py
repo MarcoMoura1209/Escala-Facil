4b0cbea0426362102ecda0ce20dd9d01938bc1d7
Marco Moura <marcomoura27052009@gmail.com>
2026-08-16 21:39:04 -0300

    Add: Criação do .env.example e atualização das configurações para usar variáveis de ambiente

--- EscalaFacil/settings/base.py
+++ EscalaFacil/settings/base.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.1/ref/settings/
 """
 
+from decouple import config
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,10 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 # See https://docs.djangoproject.com/en/6.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-jz16=ot^ippa(a=yqg2h7a643+%k+&dfm^=eah12)iy(9t5-rj'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
